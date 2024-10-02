72b0e2c06883023cf9a1684acaae702e9aa1e8ef
aakashstha1 <aakash.cerestha90@gmail.com>
2024-10-02 22:12:15 +0545

    addes sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./Login";
 import Hero from "../components/Hero";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+    <div className="container mx-auto px-4 py-3 space-y-5">
       <Hero />
+      <Sidebar />
     </div>
   );
 };
