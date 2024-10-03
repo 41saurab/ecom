6b9ebfafc59010cd35a92b5dd4f4c60c9c9d4b27
saurab <41saurab@gmail.com>
2024-10-03 23:29:23 +0545

    cards

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -3,14 +3,7 @@ import Card from "./subComponents/Card";
 
 const Products = () => {
   return (
-    <div className="shadow-lg rounded-md w-3/4 p-5 bg-light-gray grid grid-cols-4 gap-5 justify-center">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="shadow-lg rounded-md w-3/4 p-5 bg-light-gray ">
       <Card />
     </div>
   );
