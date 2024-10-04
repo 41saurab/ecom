1c9c8ca24d7e3f0bd770e222450bc8dd7fbb712c
aakashstha1 <aakash.cerestha90@gmail.com>
2024-10-04 21:26:29 +0545

    new updated

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./subComponents/Card";
 
 const Products = () => {
+  // State to track selected option (either 'Latest' or 'Popular')
+  const [selected, setSelected] = useState("Latest");
+
   return (
-    <div className="shadow-lg rounded-md w-3/4 p-5 bg-light-gray ">
-      <Card />
+    <div>
+      <div className="shadow-lg rounded-md p-5 bg-light-gray">
+        <div className="flex gap-8 text-xl text-dark font-bold pb-3">
+          {/* 'Latest' option */}
+          <h1
+            onClick={() => setSelected("Latest")}
+            className={`cursor-pointer flex gap-2 justify-center items-center ${
+              selected === "Latest" ? "border-b-4 border-dark" : ""
+            }`}
+          >
+            Latest
+            <img src="../images/latest.png" className="h-4 " />
+          </h1>
+
+          {/* 'Popular' option */}
+          <h1
+            onClick={() => setSelected("Popular")}
+            className={`cursor-pointer flex gap-2 justify-center items-center ${
+              selected === "Popular" ? "border-b-4 border-dark" : ""
+            }`}
+          >
+            Popular
+            <img src="../images/popular.png" className="h-5 " />
+          </h1>
+        </div>
+
+        {/* Horizontal line below */}
+        <hr className="p-3 border-[#acacac]" />
+
+        {/* Conditionally render Card component or filter based on selected option */}
+        <Card />
+      </div>
     </div>
   );
 };
